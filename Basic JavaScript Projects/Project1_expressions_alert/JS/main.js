380e23b49411da5360f48b3c735774a40d639276
MikeLouns <michael@cloudcade.com>
2021-10-28 14:22:35 -0700

    JS code assignment 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+function My_First_Function () {
+    var A = 20;
+    var B = 30;
+    var str = "Now this is green!";
+    var result = alert(A+B);
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+    
+}
