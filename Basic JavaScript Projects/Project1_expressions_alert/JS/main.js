060e711f300aaddebf5bbce0012afd5d497de130
MikeLouns <michael@cloudcade.com>
2021-10-29 12:26:01 -0700

    fixed a JS assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,9 +1,17 @@
+var A = "If you take 30", B =" and add 40 you get ";
+var B = B.fontcolor("blue");
+var A = A.fontcolor("green");
+var C = 30;
+var D = 40;
+var Add = (C+D);
+
 function My_First_Function () {
-    var A = 20;
-    var B = 30;
-    var str = "Now this is green!";
-    var result = alert(A+B);
-    var result = str.fontcolor("green");
-    document.getElementById("Green_Text").innerHTML = result;
+    var str = "this is green!"; //this is the text that will replace the old text on the button after press
+    var result = str.fontcolor("green"); //this changes the button text color green
+    document.getElementById("Green_Text").innerHTML = result; //this assigns the button text the id "Green Text"
     
 }
+
+window.alert("Hello there"); //this is the popup when the page is loaded
+document.write(A + B + Add); //This concatenates the variables
+
