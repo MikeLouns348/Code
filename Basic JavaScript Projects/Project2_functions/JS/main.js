ec3cc80ae74cf862faa0a0fa435e7497db51f014
MikeLouns <michael@cloudcade.com>
2021-10-29 15:49:11 -0700

    more JS homework

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function Dysfunction () {
+    var sentence = "I am learning"; //Variable 1
+    sentence += " a lot from this book!"; //what we're adding to var sentence
+    document.getElementById("Concatenate").innerHTML = sentence; //This searches for a element that has been contactentaed and uses it to replace the existing element
+}
