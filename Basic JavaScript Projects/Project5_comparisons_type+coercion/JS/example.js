12efc1d2c3dbca7ad2d2e0353dc7a87f42aee0bd
MikeLouns <michael@cloudcade.com>
2021-11-02 11:10:42 -0700

    added comments to example.js

--- Basic JavaScript Projects/Project5_comparisons_type+coercion/JS/example.js
+++ Basic JavaScript Projects/Project5_comparisons_type+coercion/JS/example.js
@@ -1,55 +1,55 @@
 function my_Function (){
-document.getElementById("Test").innerHTML = 0/0;
+document.getElementById("Test").innerHTML = 0/0; //replaces element with NaN
 }
 
 function is_Number1 (){
     var X = "This is a string";
-    document.getElementById("Test1").innerHTML = isNaN(X);
+    document.getElementById("Test1").innerHTML = isNaN(X);//replaces element with true or false
 }
 
 function is_Number2 (){
     var Z = 007;
-    document.getElementById("Test2").innerHTML = isNaN(Z);
+    document.getElementById("Test2").innerHTML = isNaN(Z); //replaces element with true or false
 }
 
 function Beyond (){
-    document.write(2E310);
+    document.write(2E310); //prints infinity
 }
 
-function the_Smallest (){
-    document.write(-2E310);
+function the_Smallest (){ 
+    document.write(-2E310); //prints negative infinity
 }
 
 function greater_Than (){
-    document.write(10 > 2);
+    document.write(10 > 2); // prints true or false
 }
 function less_Than (){
-    document.write(10 < 2);
-    console.log(10 < 2);
+    document.write(10 < 2); // prints true or false
+    console.log(10 < 2); // prints true or false in the console
 }
 
 function console_Math (){
-    console.log(2+2);
+    console.log(2+2); //does math in the console
 }
 
 function double_Equal1 (){
-    document.write(10==10);
+    document.write(10==10); //compares objects and prints true or false
 }
 
 function double_Equal2 (){
-    document.write(3==11);
+    document.write(3==11); //compares objects and prints true or false
 }
 
 function Compare1() {
     var x = "6/15/2020";
     var y = "June 15th, 2020";
-    document.write(x===y);
+    document.write(x===y); //compares objects and prints true or false
 }
 
 function Compare2() {
     var x = "6/15/2020";
     var y = "6/15/2020";
-    document.write(x===y);
+    document.write(x===y); //compares objects and prints true or false
 }
 
 function AND() {
@@ -57,7 +57,7 @@ function AND() {
     var y = 2;
     var a = 10;
     var b = 4;
-    document.write(x>y && a>b);
+    document.write(x>y && a>b); // (AND) compares objects and prints true or false depending on both sets
 }
 
 function AND2() {
@@ -65,7 +65,7 @@ function AND2() {
     var y = 10;
     var a = 10;
     var b = 4;
-    document.write(x>y && a>b);
+    document.write(x>y && a>b); // (AND) compares objects and prints true or false depending on both sets
 }
 
 function OR() {
@@ -73,7 +73,7 @@ function OR() {
     var y = 10;
     var a = 10;
     var b = 4;
-    document.write(x>y || a>b);
+    document.write(x>y || a>b); // (OR) compares objects and prints true or false depending on one set
 }
 
 function OR2() {
@@ -81,13 +81,13 @@ function OR2() {
     var y = 10;
     var a = 10;
     var b = 20;
-    document.write(x>y || a>b);
+    document.write(x>y || a>b);// (OR) compares objects and prints true or false depending on one set
 }
 
 function not_Function() {
-    document.getElementById("Not").innerHTML = !(20 > 10);
+    document.getElementById("Not").innerHTML = !(20 > 10); // checks if value is NOT true
 }
 
 function not_Function2() {
-    document.getElementById("Not2").innerHTML = !(5 > 10);
+    document.getElementById("Not2").innerHTML = !(5 > 10); // checks if value is NOT true
 }
